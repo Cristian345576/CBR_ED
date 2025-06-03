83382edb83b2d348d4a45881b649617a1755a4dc
Barquero Rodríguez <cristian.barquero@isepceu.es>
2025-06-03 18:58:01 +0200

    CBR_Commit de la segunda clase Moto y combustibles

--- src/clases/CBR_Moto.java
+++ src/clases/CBR_Moto.java
@@ -1,5 +1,56 @@
 package clases;
 
 public class CBR_Moto {
+	private String Modelo;
+	private int NumeroPlazas;
+	private int VelocidadMedia;
+	private String TipoCombustible;
+	private double Consumo;
+	private String Matricula;
+	
+	public String getMatricula() {
+		return Matricula;
+	}
+	public void setMatricula(String matricula) {
+		Matricula = matricula;
+	}
+	public String getModelo() {
+		return Modelo;
+	}
+	public void setModelo(String modelo) {
+		Modelo = modelo;
+	}
+	public int getNumeroPlazas() {
+		return NumeroPlazas;
+	}
+	public void setNumeroPlazas(int numeroPlazas) {
+		NumeroPlazas = numeroPlazas;
+	}
+	public int getVelocidadMedia() {
+		return VelocidadMedia;
+	}
+	public void setVelocidadMedia(int velocidadMedia) {
+		VelocidadMedia = velocidadMedia;
+	}
+	public String getTipoCombustible() {
+		return TipoCombustible;
+	}
+	public void setTipoCombustible(String tipoCombustible) {
+		TipoCombustible = tipoCombustible;
+	}
+	public double getConsumo() {
+		return Consumo;
+	}
+	public void setConsumo(double consumo) {
+		Consumo = consumo;
+	}
+	@Override
+	public String toString() {
+		return "CBR_Moto [Matricula=" + Matricula + ", Modelo=" + Modelo + ", NumeroPlazas=" + NumeroPlazas
+				+ ", VelocidadMedia=" + VelocidadMedia + ", TipoCombustible=" + TipoCombustible + ", Consumo=" + Consumo
+				+ "]";
+	}
+
+	
 
 }
---
+++ src/clases/CBR_TipoDeCombustible.java
@@ -0,0 +1,20 @@
+package clases;
+
+public class CBR_TipoDeCombustible {
+	public enum TipoCombustible {
+		GASOLINA(1.60),
+		DIESEL(1.45),
+		ELECTRICO(0.20);
+		
+		private final double precioPorLitro;
+		
+		TipoCombustible(double precioPorLitro) {
+			this.precioPorLitro = precioPorLitro;
+		}
+		
+		public double getPrecioPorLitro() {
+			return precioPorLitro;
+		}
+	}
+
+}
